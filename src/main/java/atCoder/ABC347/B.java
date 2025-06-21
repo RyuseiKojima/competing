f12898d2cdf4284a0ce027774364c51c059a5ac8
RyuseiKojima <jiko.likegc@gmail.com>
2025-06-21 17:53:05 +0900

    ABC347 B

---
+++ src/main/java/atCoder/ABC347/B.java
@@ -0,0 +1,25 @@
+package atCoder.ABC347;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<String> sets = new ArrayList<>();
+        String s = sc.next();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String val = s.substring(i, j+1);
+                if (!sets.contains(val)) {
+                    sets.add(val);
+                }
+            }
+        }
+
+        System.out.println(sets.size());
+    }
+}
