b90fbef8730723d8f0e23f9903b70f2534f5f03a
RyuseiKojima <jiko.likegc@gmail.com>
2025-08-02 22:43:57 +0900

    ABC417 A

---
+++ src/main/java/atCoder/ABC417/A.java
@@ -0,0 +1,20 @@
+package atCoder.ABC417;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String s = sc.next();
+
+        String result = s.substring(a, n - b);
+
+        System.out.println(result);
+        sc.close();
+    }
+}
