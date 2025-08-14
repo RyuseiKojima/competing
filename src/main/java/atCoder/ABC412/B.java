09ca7324bdb433b2096f93a6017b8baa8e482d56
RyuseiKojima <jiko.likegc@gmail.com>
2025-08-14 10:51:16 +0900

    Add solution for problem B in ABC412

---
+++ src/main/java/atCoder/ABC412/B.java
@@ -0,0 +1,22 @@
+package atCoder.ABC412;
+import java.util.*;
+
+class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A < B) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
