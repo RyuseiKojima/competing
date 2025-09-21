4221e3e40556f4d8e87cf7b69bf546e43a662bda
RyuseiKojima <jiko.likegc@gmail.com>
2025-09-21 09:16:33 +0900

    Add solution for problem A in ABC424

---
+++ src/main/java/atCoder/ABC424/A.java
@@ -0,0 +1,17 @@
+package atCoder.ABC424;
+
+import java.util.Scanner;
+
+class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println(A == B || B == C || C == A ? "Yes" : "No");
+
+        sc.close();
+    }
+}
