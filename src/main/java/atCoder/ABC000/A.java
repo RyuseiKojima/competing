d6f1ec9e8ce190a6283e061eecad8a93346f99ac
RyuseiKojima <jiko.likegc@gmail.com>
2026-01-18 09:28:59 +0900

    テンプレ作成

---
+++ src/main/java/atCoder/ABC000/A.java
@@ -0,0 +1,11 @@
+package atCoder.ABC000;
+
+import java.util.Scanner;
+
+class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
---
+++ src/main/java/atCoder/ABC000/D.java
@@ -0,0 +1,11 @@
+package atCoder.ABC000;
+
+import java.util.Scanner;
+
+class D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
